60f46ce89add2dc6efc78faa4472945c56c17051
dsalas21 <125444146+dsalas21@users.noreply.github.com>
2024-06-11 22:34:13 -0700

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 import express from'express'
 import { pool } from './db.js';
+import { PORT } from './config.js';
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Backend is running');
   });
 
+  app.listen(PORT, "0.0.0.0", function () {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  });
+  
 
 app.get('/test-db-connection', async (req, res) => {
     try {
